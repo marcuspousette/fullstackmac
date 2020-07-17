c4bae6f1acbec7056591a82b95cddbf0e21a79ce
marcuspousette <marcuspousette@gmail.com>
2020-07-17 16:41:59 +0200

    new logo

--- src/components/landing-page/Landing_page.js
+++ src/components/landing-page/Landing_page.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { Header, Container, Image } from 'semantic-ui-react';
 import Burger from '../burger/Burger.js';
 import './Landing_page.scss';
-
-const ImageExampleImage = () => <Image src="/fullstackmac-logo.svg" size="large" className="Landing_page__logo" />;
+import Logo from '../logo/Logo.js';
 
 class Landing_page extends React.Component {
 	render() {
 		return (
 			<div id="Landing_page">
+				<Burger />
 				<Container className="Landing_page__background">
 					<Header as="h1" className="Landing_page__header">
 						<span className="Landing_page__header--first">MARCUS</span>
 						<span className="Landing_page__header--second">POUSETTE</span>
 					</Header>
-					<Burger />
-					<ImageExampleImage />
+					<Logo classes="Landing_page__logo" />
 				</Container>
 			</div>
 		);
