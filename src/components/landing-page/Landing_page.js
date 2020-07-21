3a4c18c88a3afbd1bd29579648e021ad2d88560f
marcuspousette <marcuspousette@gmail.com>
2020-07-21 08:34:32 +0200

    new imges

--- src/components/landing-page/Landing_page.js
+++ src/components/landing-page/Landing_page.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Header, Container, Image } from 'semantic-ui-react';
+import gsap from 'gsap';
+import { Header, Container, Image, FormButton } from 'semantic-ui-react';
 import Burger from '../burger/Burger.js';
 import './Landing_page.scss';
 import Logo from '../logo/Logo.js';
 
+const logStyle = {
+	text: {
+		opacity: 0
+	}
+};
+
 class Landing_page extends React.Component {
+	componentDidMount() {
+		const logoAnimation = gsap.timeline();
+		logoAnimation
+			.fromTo('#right-horn', { x: -80 }, { x: 0, duration: 0.5, delay: 2 })
+			.fromTo('#left-horn', { x: 80 }, { x: 0, duration: 0.5 }, '<')
+			.to('#fullstackmac-text', { opacity: 1, duration: 0.5 });
+	}
+
 	render() {
 		return (
 			<div id="Landing_page">
 				<Burger />
 				<Container className="Landing_page__background">
+					<div className="Landing_page__background-image"></div>
 					<Header as="h1" className="Landing_page__header">
 						<span className="Landing_page__header--first">MARCUS</span>
 						<span className="Landing_page__header--second">POUSETTE</span>
 					</Header>
-					<Logo classes="Landing_page__logo" />
+					<Logo classes="Landing_page__logo" style={logStyle} />
 				</Container>
 			</div>
 		);
